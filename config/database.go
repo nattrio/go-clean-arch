365bb536b308693c12d75a1ad86487d9364d208c
nattrio <jirapat.nattrio@gmail.com>
2023-07-05 11:07:13 +0700

    feat(gorm): :sparkles: Add SqlLogger

--- config/database.go
+++ config/database.go
@@ -1,18 +1,32 @@
 package config
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/nattrio/go-clean-arch/pkg/utils"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
+
+type SqlLogger struct {
+	logger.Interface
+}
+
+func (l SqlLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
+	sql, _ := fc()
+	fmt.Printf("%v\n==========================\n", sql)
+}
 
 func ConnectionDB(config *Config) *gorm.DB {
 	sqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Bangkok",
 		config.DBHost, config.DBPort, config.DBUsername, config.DBPassword, config.DBName)
 
-	db, err := gorm.Open(postgres.Open(sqlInfo), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(sqlInfo), &gorm.Config{
+		Logger: &SqlLogger{},
+	})
 	utils.ErrorPanic(err)
 
 	fmt.Println("Connected to database successfully")
